8647f726e20dba9618b4242bd0dbf356eb7f7194
Antonio Marrero Bonilla <gaming_marrero@yahoo.com>
2018-01-28 02:09:07 -0500

    Cleaned Up

--- src/app/oauthserver/oauthserver.component.ts
+++ src/app/oauthserver/oauthserver.component.ts
@@ -6,18 +6,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './oauthserver.component.html',
   styleUrls: ['./oauthserver.component.css']
 })
+
 export class OauthserverComponent implements OnInit {
-  userInfo = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  Login() {
-    // tslint:disable-next-line:max-line-length
-    this.userInfo = '<href="https://mixer.com/oauth/authorize?response_type=token&redirect_uri=http://hyperchucksgaming.com&scope=user:details:self&client_id=f4d214efea81457623beb93ce18d941c4e997be681350dbd"';
-    return this.userInfo;
-  }
+  // Here we will put the login information for login.
+  Login() {}
 
 }
